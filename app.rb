8aff50f9217597b1b4bd25492c7d4726c71cc3f0
slavikpl <pleskachev777@gmail.com>
2019-03-05 00:27:11 +0300

    add about, contacts, visit page

--- app.rb
+++ app.rb
@@ -6,3 +6,19 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+get '/about' do
+    erb :about
+end
+
+get '/something' do
+  "Hello World"
+end
+
+get '/visit' do
+    erb :visit
+end
+
+get '/contacts' do
+    erb :contacts
+end
