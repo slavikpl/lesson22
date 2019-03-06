19d12d436150aff37aaef95bf181c1b8b73dfa88
slavikpl <pleskachev777@gmail.com>
2019-03-07 00:30:13 +0300

    Homework lasson22

--- app.rb
+++ app.rb
@@ -19,6 +19,37 @@ get '/visit' do
     erb :visit
 end
 
+post '/visit' do
+	    @username = params[:username]
+		@phone = params[:phone]
+		@datetime = params[:datetime]
+		@namemaster = params[:namemaster]
+		@title="Thank you"
+		
+		
+
+		f = File.open './public/user.txt', 'a'
+	         f.write "User: #{@username}, Phone: #{@phone}, Date and time: #{@datetime}\n Сотрудник: #{@namemaster}\n "
+	         f.close
+  
+	         erb :message
+end
+
+	
+
 get '/contacts' do
     erb :contacts
+end
+
+post '/contacts' do
+	    @email = params[:email]
+		@message= params[:message]
+		
+		@title="<h2>Спасибо за Ваше обращение!</h2>"
+
+		f = File.open './public/contacts.txt', 'a'
+	         f.write "Email: #{@email}, message: #{@message}\n"
+	         f.close
+  
+	         erb :message
 end
